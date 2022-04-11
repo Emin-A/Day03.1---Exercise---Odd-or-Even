35ae5279469ec218522ab8648d9cdbc671e1b84a
Emin Avdovic <72197800+Emin-A@users.noreply.github.com>
2022-04-11 15:38:45 +0000

    Broke the code into simple terms.

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # Odd or Even   ( % - Modulo will give the remainder of two divided numbers)
+# 7 % 2  is same as 2 + 2 + 2 + 1
+
+number = int(input("Which number do you want to change?"))
+# odd_number = number % 2 == 0
+# even_number = number % 2 == 1
+
+# Check if the number is odd or even
+
+if number % 2 == 0:
+  print("This is an even number!")
+else:
+  print("This is an odd number!")
+
+# if number == odd_number:
+#   print("This is an odd number!")
+# elif number == even_number:
+#   print("This is an even number!")
